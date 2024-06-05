ba92269fc6b6fc65719f1d8e492535bc407ac55c
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2024-06-05 13:12:12 +0300

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -5,6 +5,6 @@ $parool='';
 $andmebaas='projekt';
 $conn=new mysqli($serverinimi, $kasutaja, $parool, $andmebaas);
 if ($conn->connect_error) {
-    die("Ошибка подключения: " . $conn->connect_error);
+    die("Ühendus viga: " . $conn->connect_error);
 }
 ?>
